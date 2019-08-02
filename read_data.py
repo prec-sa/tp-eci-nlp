c7eb15db36e97e096a6fc7fac7826f22783098b7
Bardiego <42683164+Bardiego@users.noreply.github.com>
2019-08-01 23:08:10 -0300

    read_data_diegox

--- read_data.py
+++ read_data.py
@@ -2,6 +2,7 @@
 import argparse
 import json
 import csv
+import string
 
 def main():
     ap = argparse.ArgumentParser()
@@ -11,18 +12,25 @@ def main():
     args = ap.parse_args()
 
     sentence_data = open(args.sentences, 'r')
+    csv_out=open("data_val.csv","w")
+    mywriter=csv.writer(csv_out,delimiter=" ")
+    
+    
+    
     if args.labels:
         label_data = open(args.labels, 'r')
         for sentence, label in zip(it_sentences(sentence_data), it_labels(label_data)):
             # Tenemos la oración en sentence con su categoría en label
-            #print(label, sentence)
+            sentence = sentence.lower()
+            output="__label__"+label,sentence.translate(str.maketrans('', '', string.punctuation))
+            mywriter.writerow(output)
             pass
     else:
-        for sentence in it_sentences(sentence_data):
-            # Tenemos una oración en sentence
+        for sentence in it_sentences(sentence_data):            
+        # Tenemos una oración en sentence
             #print(sentence)
-            pass
-    
+            pass        
+
 
 def it_sentences(sentence_data):
     for line in sentence_data:
@@ -33,8 +41,7 @@ def it_labels(label_data):
     label_data_reader = csv.DictReader(label_data)
     for example in label_data_reader:
         yield example['gold_label']
-
-
+        
 
 
 main()
